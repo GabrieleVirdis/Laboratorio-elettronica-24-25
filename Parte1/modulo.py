3f6086d4be45761cbb754b13276db887c3b23b5c
GabrieleVirdis <gabriele.virdis@studenti.unipg.it>
2025-02-28 15:09:39 +0100

    Update modulo.py

--- Parte1/modulo.py
+++ Parte1/modulo.py
@@ -3,7 +3,7 @@ import numpy as np
 def idft(X):
     N = len(X)                                # Numero totale di coefficienti
     indici = np.arange(0, N)             # Indici dei campioni da calcolare
-    sintesi = np.empty(len(indices), dtype=complex)  # Array per salvare i risultati
+    sintesi = np.empty(len(indici), dtype=complex)  # Array per salvare i risultati
     
     # Calcolo dell'IDFT 
     k = np.arange(N)                          # Indici per i coefficienti in frequenza
